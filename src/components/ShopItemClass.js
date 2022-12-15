4ee9912f8fa7a7027191ee33ee4531616ef0852c
ozkostay <ozkostay@mail.ru>
2022-12-15 22:54:34 +0500

    class -1 151222

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+export default class ShopItemClass extends React.Component{
+
+  render() {
+    const item = this.props.item;
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
